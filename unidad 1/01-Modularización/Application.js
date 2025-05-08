4392a090dd45e355484730b77d789a460001d782
Fuentes Leonel <lmfuentes00@gmail.com>
2025-05-08 15:13:41 -0300

    2025/05/08 - Agrega ejericios

---
+++ unidad 1/01-Modularización/Application.js
@@ -0,0 +1,33 @@
+import { LoginApplicationView } from './LoginApplicationView.js';
+
+class Application
+{
+	constructor( apiInstanceObject )
+	{
+		this._api = apiInstanceObject;
+		this._defaultView = new LoginApplicationView(this._api);		
+		this._maxLoginFailedAttempts = this._api.getMaxLoginAttempts();
+		this._attempts = 0;
+		this._api_return = null;
+	}
+
+	init()
+	{
+		this._api_return = this._defaultView.show();
+	}
+
+	run()
+	{
+		while( this._api_return.result == 'USER_PASSWORD_FAILED' && this._attempts < this._maxLoginFailedAttempts )
+		{
+			this._api_return = this._defaultView.show();
+
+			if ( this._api_return.result == 'USER_PASSWORD_FAILED' )
+			{
+				this._attempts++;
+			}
+		}
+	}
+}
+
+export { Application };
